81baa53997efbc8f68fd32b15114c3429731b90d
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2025-09-16 02:30:14 +0300

    vdom

---
+++ packages/runtime/src/h.js
@@ -0,0 +1,52 @@
+import { withoutNulls } from "./utils/arrays";
+
+export const DOM_TYPES = {
+  TEXT: "text",
+  ELEMENT: "element",
+  FRAGMENT: "fragment",
+};
+
+export function h(tag, props = {}, children = []) {
+  return {
+    tag,
+    props,
+    children: mapTextNodes(withoutNulls(children)),
+    type: DOM_TYPES.ELEMENT,
+  };
+}
+
+export function hString(str) {
+  return { type: DOM_TYPES.TEXT, value: str };
+}
+
+export function hFragment(vNodes) {
+  return {
+    type: DOM_TYPES.FRAGMENT,
+    children: mapTextNodes(withoutNulls(vNodes)),
+  };
+}
+
+function mapTextNodes(children) {
+  return children.map((child) =>
+    typeof child === "string" ? hString(child) : child
+  );
+}
+
+/*
+function lipsum(n) {
+  const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+    ut aliquip ех еа commodo consequat`;
+
+  return hFragment(Array(n).fill(h("p", {}, [text])));
+}
+*/
+
+/*
+function MessageComponent({ level, message }) {
+  return h("div", { class: `message message--${level}` }, [
+    h("p", {}, [message]),
+  ]);
+}
+*/
